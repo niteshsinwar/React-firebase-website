6e37ce448df4e57899bebb68e6af79ced1f2ddf9
Nitesh Sinwar <niteshsinwar@gmail.com>
2022-07-06 18:01:53 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,10 @@
 export const config = {
-  apiKey: "AIzaSyA_h3MxdbqR3OZRUPfw66lFSIvOY7TTLRk",
+  apiKey: "***********",
   authDomain: "cred-restaurant.firebaseapp.com",
   projectId: "cred-restaurant",
   storageBucket: "cred-restaurant.appspot.com",
-  messagingSenderId: "554420553965",
-  appId: "1:554420553965:web:e6223d965ee70dca77a4e4",
-  measurementId: "G-QZBC6CNY4L"
+  messagingSenderId: "**********",
+  appId: "**********",
+  measurementId: "**********"
 };
 
